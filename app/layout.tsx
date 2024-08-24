789560b1fb7b8fccb5c280a2730186eba7df9b0e
Robert Washbourne <rawashbourne@gmail.com>
2024-08-24 12:19:59 +0000

    padding foot

--- app/layout.tsx
+++ app/layout.tsx
@@ -39,8 +39,8 @@ export default function RootLayout({
             </main>
           </div>
 
-          <footer className="bg-white shadow-sm mt-8">
-            <div className="container max-w-3xl mx-auto py-4 px-4 text-left text-gray-600 font-normal">
+          <footer className="mt-8">
+            <div className="container max-w-3xl mx-auto py-4 px-4 text-left text-gray-600 font-normal px-8">
               © 2024 rawsh. All rights reserved.
             </div>
           </footer>
