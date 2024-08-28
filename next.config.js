b1f2dcc3613ab8c2fe517e9b747be79477a0f819
Robert Washbourne <rawashbourne@gmail.com>
2024-08-28 03:22:51 +0000

    updoot

--- next.config.js
+++ next.config.js
@@ -2,6 +2,20 @@ const { withContentlayer } = require('next-contentlayer')
 
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    async rewrites() {
+      return [
+        {
+            source: '/rawbot',
+            destination: 'https://lichess.org/@/rawbot/playing',
+        },
+        {
+            source: '/assets/:path*',
+            destination: 'https://lichess.org/assets/:path*'
+        }
+      ];
+    }
+  };
+  
 
 module.exports = withContentlayer(nextConfig)
