be3799f1254c36da76d8cccd5219c524d030a89f
Robert Washbourne <rawashbourne@gmail.com>
2024-11-20 20:50:29 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -10,8 +10,8 @@ export default function Home() {
   return (
     <div className="grid grid-cols-1 gap-2">
       {sortedPosts.map((post, idx) => (
-        <div className='px-4 py-2 text-xl' key={idx}>
-          <div>
+        <div className='px-2 py-2 text-xl' key={idx}>
+          <div className='text-balance'>
             <Link 
               href={post.url} 
               className="text-slate-600 hover:text-pink-400 transition-colors duration-100 font-medium"
