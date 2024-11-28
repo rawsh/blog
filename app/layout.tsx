f09bf83a557acfcaafcee78b2eb9fc0d00f69ada
Robert Washbourne <rawashbourne@gmail.com>
2024-11-27 23:43:28 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -60,10 +60,10 @@ export default function RootLayout({
               <div className="flex-2 text-balance">© 2024 Robert Washbourne. All rights reserved.</div>
               <Link 
                 href="https://raw.sh/feed.xml" 
-                className="inline-flex gap-2 items-center hover:text-pink-400 transition-colors duration-100 font-medium"
+                className="inline-flex flex-[0] gap-2 items-center hover:text-pink-400 transition-colors duration-100 font-medium"
               >
-                <span className="">Subscribe with RSS</span>
-                <Rss className="" size={18} />
+                <span className="flex-[0]">Subscribe with RSS</span>
+                <Rss className="flex-[0]" size={18} />
               </Link>
             </div>
           </div>
